f84f99d944a993b3f4397856c979f575d7938f46
LanceNightfall <cdude9000@gmail.com>
2017-05-24 12:29:50 -0500

    Add files via upload

---
+++ ClearMeasureHomeworkC#.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibTest12;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(LibTest12.FizzBuzz.FzzBzz());
+        }
+    }
+}
